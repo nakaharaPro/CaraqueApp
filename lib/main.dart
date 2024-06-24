e9a99d1402408c9c989d41375a7f881d4b9d257d
nakahara <marimo09300130@gmail.com>
2024-06-24 22:02:07 +0900

    修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'dart:async';
+import 'package:caraqueprod/flavors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 
+import 'package:caraqueprod/gen/firebase_options_dev.dart' as dev;
+import 'package:caraqueprod/gen/firebase_options_prod.dart' as prod;
+
 FutureOr<void> main() async {
+  final flavor = F.appFlavor!;
+  final options = (flavor == Flavor.dev) ? dev.DefaultFirebaseOptions.currentPlatform : prod.DefaultFirebaseOptions.currentPlatform;
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: options);
+
   runApp(const App());
 }
