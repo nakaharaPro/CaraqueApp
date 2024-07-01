a4de2a4fb18b9a7b9565a3b299cc15d7728fa9bf
nakahara <marimo09300130@gmail.com>
2024-07-01 22:23:44 +0900

    googleMapAPI

--- lib/view/pages/my_home_page/compornents/main_screen/components/order_screen.dart
+++ lib/view/pages/my_home_page/compornents/main_screen/components/order_screen.dart
@@ -1,17 +1,66 @@
-//注文ページ
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class OrderScreen extends StatelessWidget{
+class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-      appBar: AppBar(
-        title: const Text('注文ページ'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Maps',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: const Center(
-          child: Text('注文ページ画面', style: TextStyle(fontSize: 32.0))),
+      home: const MapView(),
+    );
+  }
+}
+
+class MapView extends StatefulWidget {
+  const MapView({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  // マップビューの初期位置
+  final CameraPosition _initialLocation = const CameraPosition(
+    target: LatLng(35.6895, 139.6917), // 東京の座標を初期位置として設定
+    zoom: 10,
+  );
+
+  // マップの表示制御用
+  late GoogleMapController mapController;
+
+  @override
+  Widget build(BuildContext context) {
+    // 画面の幅と高さを取得
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SizedBox(
+        height: height,
+        width: width,
+        child: Stack(
+          children: <Widget>[
+            GoogleMap(
+              initialCameraPosition: _initialLocation,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              mapType: MapType.normal,
+              zoomGesturesEnabled: true,
+              zoomControlsEnabled: false,
+              onMapCreated: (GoogleMapController controller) {
+                mapController = controller;
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
