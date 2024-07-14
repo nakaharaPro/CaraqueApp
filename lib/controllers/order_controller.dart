5f838cc0d37240a7331c6e3836713aa1e5a021e9
nakahara <marimo09300130@gmail.com>
2024-07-14 19:38:51 +0900

    桁表示

--- lib/controllers/order_controller.dart
+++ lib/controllers/order_controller.dart
@@ -4,17 +4,18 @@ class OrderController extends GetxController {
   static OrderController get to => Get.find<OrderController>();
 
   Map<String, Map<String, int>> quantities = {};
-  int totalAmount = 0;
+ 
 
-  Future<int> amountCalculation() async {
-    quantities.forEach((key, value) {
+  int amountCalculation(){
+  int totalAmount = 0; //初期化
+   quantities.forEach((key, value) {
       //keyに商品名 valueにMap
       print('Key : $key');
       if (key == '生デコホール' || key == '生チョコホール') {
         value.forEach((sizekey, quantityvalue) {
           //keyサイズ　valueに個数
           if (sizekey == '12cm') {
-            totalAmount = totalAmount + quantityvalue * 2350;
+           totalAmount = totalAmount + quantityvalue * 2350;
           }
           if (sizekey == '15cm') {
             totalAmount = totalAmount + quantityvalue * 3550;
@@ -51,7 +52,29 @@ class OrderController extends GetxController {
           }
         });
       }
-      
+
+      if (key == 'パリパリショコラ') {
+        value.forEach((sizekey, quantityvalue) {
+          //keyサイズ　valueに個数
+          if (sizekey == '12cm') {
+            totalAmount = totalAmount + quantityvalue * 2450;
+          }
+          if (sizekey == '15cm') {
+            totalAmount = totalAmount + quantityvalue * 3700;
+          }
+          if (sizekey == '18cm') {
+            totalAmount = totalAmount + quantityvalue * 4700;
+          }
+          if (sizekey == '21cm') {
+            totalAmount = totalAmount + quantityvalue * 6150;
+          }
+          if (sizekey == '24cm') {
+            totalAmount = totalAmount + quantityvalue * 7400;
+          }
+        });
+      }
+
+
     });
 
     return totalAmount;
--- lib/view/pages/my_home_page/compornents/main_screen/components/order_screen.dart
+++ lib/view/pages/my_home_page/compornents/main_screen/components/order_screen.dart
@@ -2,6 +2,8 @@ import 'package:caraqueprod/constant/colors_const.dart';
 import 'package:caraqueprod/constant/hole_products_discription.dart';
 import 'package:caraqueprod/controllers/order_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key});
@@ -12,49 +14,70 @@ class OrderScreen extends StatefulWidget {
 
 class _OrderPageState extends State<OrderScreen> {
   final List<String> products = ['生デコホール', '生チョコホール', '栗チョコホール', 'パリパリショコラ'];
-  final List<String> holeDiscription = HoleProductsDiscription.holeProductDescription;
+  final List<String> holeDiscription =
+      HoleProductsDiscription.holeProductDescription; //商品説明
   final Map<String, Map<String, int>> quantities = {};
-  final List<String> sizes = ['12cm', '15cm', '18cm','21cm', '24cm'];
+  final List<String> sizes = ['12cm', '15cm', '18cm', '21cm', '24cm'];
+  final formatter = NumberFormat("#,###");
+
 
   @override
   void initState() {
     super.initState();
-    for (var product in products) {//productsリストから1つずつproductに入れ、length分繰り返し
-      quantities[product] = {'12cm': 0, '15cm': 0, '18cm': 0,'21cm': 0,'24cm': 0};
+    for (var product in products) {
+      //productsリストから1つずつproductに入れ、length分繰り返し
+      quantities[product] = {
+        '12cm': 0,
+        '15cm': 0,
+        '18cm': 0,
+        '21cm': 0,
+        '24cm': 0
+      };
     }
   }
 
   @override
   Widget build(BuildContext context) {
     final orderController = OrderController.to;
-    var totalAmount;
+    int? totalAmount;
+    String outputTotalAmount = '0';
 
     return Scaffold(
-   appBar: AppBar(
-        title: const Text("ホールケーキ注文",style: TextStyle(fontSize:25.0,color: Colors.white),textAlign: TextAlign.left),
-        toolbarHeight: 40.0,//AppBarの高さ
-        backgroundColor:ColorsConst.ColorGrey,//色
+      appBar: AppBar(
+        title: const Text("ホールケーキ注文",
+            style: TextStyle(fontSize: 25.0, color: Colors.white),
+            textAlign: TextAlign.left),
+        toolbarHeight: 40.0, //AppBarの高さ
+        backgroundColor: ColorsConst.ColorGrey, //色
       ),
       body: ListView.builder(
-        itemCount: products.length,//商品名レングス
+        itemCount: products.length, //商品名レングス
         itemBuilder: (context, index) {
-          return ExpansionTile(//開閉
-            title:Column(
+          return ExpansionTile(
+            //開閉
+            title: Column(
               children: [
-             Text(products[index],style: const TextStyle(fontSize: 20.0 ,fontWeight: FontWeight.bold),),
-             Text(holeDiscription[index],style: const TextStyle(fontSize: 10.0)),
+                Text(
+                  products[index],
+                  style: const TextStyle(
+                      fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                Text(holeDiscription[index],
+                    style: const TextStyle(fontSize: 10.0)),
               ],
             ),
-            children: sizes.map((size) {//mapはsized配列から1つ1つ要素を取り出し(size)に渡して処理する
+            children: sizes.map((size) {
+              //mapはsized配列から1つ1つ要素を取り出し(size)に渡して処理する
               return ListTile(
                 title: Text(size),
                 trailing: Row(
-                  mainAxisSize: MainAxisSize.min,//右端に配置
+                  mainAxisSize: MainAxisSize.min, //右端に配置
                   children: [
                     IconButton(
-                      icon: const Icon(Icons.remove),//マイナスボタン
+                      icon: const Icon(Icons.remove), //マイナスボタン
                       onPressed: () {
-                        setState(() {//マイナス処理
+                        setState(() {
+                          //マイナス処理
                           if (quantities[products[index]]![size]! > 0) {
                             quantities[products[index]]![size] =
                                 quantities[products[index]]![size]! - 1;
@@ -62,12 +85,14 @@ class _OrderPageState extends State<OrderScreen> {
                         });
                       },
                     ),
-                    Text(quantities[products[index]]![size]!.toString()),//カウント
+
+                    Text(quantities[products[index]]![size]!.toString()), //カウント
 
                     IconButton(
                       icon: const Icon(Icons.add),
                       onPressed: () {
-                        setState(() {//プラス処理
+                        setState(() {
+                          //プラス処理
                           quantities[products[index]]![size] =
                               quantities[products[index]]![size]! + 1;
                         });
@@ -76,7 +101,7 @@ class _OrderPageState extends State<OrderScreen> {
                   ],
                 ),
               );
-            }).toList(),//ListTitleのMapをListに格納する[Map,Map,Map]
+            }).toList(), //ListTitleのMapをListに格納する[Map,Map,Map]
           );
         },
       ),
@@ -84,39 +109,42 @@ class _OrderPageState extends State<OrderScreen> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: ElevatedButton(
-            child: const Text('注文する'),
+            child:const Text('注文する'),
+
             onPressed: () {
-              showDialog(//ダイアログ表示
+              orderController.quantities = quantities;
+              totalAmount = orderController.amountCalculation();//合計金額計算
+              outputTotalAmount = formatter.format(totalAmount);
+              //合計金額の処理
+              showDialog(
+                //ダイアログ表示
                 context: context,
                 builder: (context) => AlertDialog(
                   title: const Text('注文内容'),
-                  content: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: quantities.entries.map((entry) {
-                      return Column(
-                        children: [
-                          Text(entry.key),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: entry.value.entries
-                                .map((sizeEntry) => Text(
-                                    '${sizeEntry.key}: ${sizeEntry.value}'))
-                                .toList(),
-                          ),
-                        ],
-                      );
-                    }).toList(),
-                  ),
+                  content: Text('合計金額 : ¥$outputTotalAmount'),
+                  // content: Column(
+                  //   mainAxisSize: MainAxisSize.min,
+                  //   children: quantities.entries.map((entry) {
+                  //     return Column(
+                  //       children: [
+                  //         Text(entry.key),
+                  //         Column(
+                  //           crossAxisAlignment: CrossAxisAlignment.start,
+                  //           children: entry.value.entries
+                  //               .map((sizeEntry) => Text(
+                  //                   '${sizeEntry.key}: ${sizeEntry.value}'))
+                  //               .toList(),
+                  //         ),
+                  //       ],
+                  //     );
+                  //   }).toList(),
+                  // ),
                   actions: [
                     TextButton(
                       child: const Text('OK'),
                       onPressed: () {
                         //コントローラーに入力値を格納して計算を行い、別画面へ合計金額、注文内容を記載したメールを両者に飛ばす
-                        orderController.quantities =  quantities;
-                        print(orderController.quantities);
-                        totalAmount = orderController.amountCalculation();
-                        print('合計金額  $totalAmount');
-                        
+
                         Navigator.of(context).pop(); // ダイアログを閉じる
                       },
                     ),
@@ -130,4 +158,3 @@ class _OrderPageState extends State<OrderScreen> {
     );
   }
 }
-
