526b338769b4b58de1231d19c181dfaa37c446c2
nakahara <marimo09300130@gmail.com>
2024-07-17 21:54:42 +0900

    修正

---
+++ lib/view/pages/OTAMESI/authentication.dart
@@ -0,0 +1,107 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Authentication {
+  // Firebase initialization
+  static Future<FirebaseApp> initializeFirebase(
+      {required BuildContext context}) async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();//初期化
+    //初期化には、Firebaseプロジェクトの設定情報（Googleサービスの設定ファイルなど）が必要です。
+    //この情報は通常、google-services.json（Android）やGoogleService-Info.plist（iOS）
+    //としてプロジェクトに含まれています。
+
+    User? user = FirebaseAuth.instance.currentUser;//現在のログインユーザー　なければnull
+
+    if (user != null) {
+      Navigator.of(context).pushReplacement(//.of(context)のページを削除してUserInfoScreenに遷移
+        MaterialPageRoute(
+          builder: (context) => UserInfoScreen(user: user,),
+        ),
+      );
+    }
+    return firebaseApp;
+  }
+
+  // GoogleSignIn
+  static Future<User?> signInWithGoogle({required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      try {
+        final UserCredential userCredential =
+            await auth.signInWithCredential(credential);
+
+        user = userCredential.user;
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'account-exists-with-different-credential') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            Authentication.customSnackBar(
+              content:
+                  'The account already exists with a different credential.',
+            ),
+          );
+        } else if (e.code == 'invalid-credential') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            Authentication.customSnackBar(
+              content: 'Error occurred while accessing credentials. Try again.',
+            ),
+          );
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          Authentication.customSnackBar(
+            content: 'Error occurred using Google Sign-In. Try again.',
+          ),
+        );
+      }
+    }
+    return user;
+  }
+
+  // GoogleSignOut
+  static Future<void> signOut({required BuildContext context}) async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    try {
+      if (!kIsWeb) {
+        await googleSignIn.signOut();
+      }
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        Authentication.customSnackBar(
+          content: 'Error signing out. Try again.',
+        ),
+      );
+    }
+  }
+
+  //ErrorSnackBar
+  static SnackBar customSnackBar({required String content}) {
+    return SnackBar(
+      backgroundColor: Colors.black,
+      content: Text(
+        content,
+        style: const TextStyle(color: Colors.redAccent, letterSpacing: 0.5),
+      ),
+    );
+  }
+}
+
